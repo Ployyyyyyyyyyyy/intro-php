21abe884a7a3c3fe089dc6cf1defc22d5edd3de3
Ployyyyyyyyyyyy <152580337+Ployyyyyyyyyyyy@users.noreply.github.com>
2024-01-19 11:25:01 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>IntroPHP</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+<body>
+<h1 class="fs-1 text-danger text-center">
+        <?php echo "แนะนำตัวเอง" ?>
+   </h1> 
+
+   <h1 class="fs-3 text-primary text-center">
+        <?php echo "พัชรพร อุสาพรหม 249911 เลขที่ 13 ม.5/6" ?>
+   </h1>
+   
+   <h1 class="fs-3 text-primary text-center">
+        <?php echo "สิ่งที่ชอบทาน : ส้มตำ ก้อย สุกี้ หมูกะทะ" ?>
+   </h1>
+
+   <h1 class="fs-3 text-primary text-cener">
+        <?php echo "สเปค : สูง เข้าใจกัน" ?>
+   </h1>
+
+   <h1 class="fs-3 text-primary text-center">
+        <?php echo "สีที่ชอบ : สีชมพู ฟ้า เหลือง ม่วงพาสเทล เขียวนีออน แดง ขาว ดำ" ?>
+   </h1>
+
+   <h1 class="fs-3 text-primary text-center">
+         <?php echo "งานอดิเรก : ฟังเพลง นอน เล่นเกม ดูหนัง ไปเที่ยว ระบายสี" ?>
+            
+         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+  </body>
+</html>
